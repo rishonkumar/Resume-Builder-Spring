05bb14bc1c6d3a9cac880dfc3096a1eadbccbcfd
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-05-05 15:59:22 +0530

    Adding User and User repository

---
+++ src/main/java/com/resumeportal/resumeportal/HomeController.java
@@ -0,0 +1,9 @@
+package com.resumeportal.resumeportal;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+}
---
+++ src/test/java/com/resumeportal/resumeportal/ResumePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeportal.resumeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
